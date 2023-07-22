4544166b55f08e15dcacd6b43bef749c36a7a350
Nimit <nimitzakharia@gmail.com>
2023-07-22 16:25:24 +0530

    fixed the bug with the products checkout page(not deleting in order)

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -10,12 +10,14 @@ function Home() {
             <img  alt="prime-add-img" className="Home__img" src='https://m.media-amazon.com/images/I/71JFH2uM8JL._SX3000_.jpg'/>
             <div className='Home__row'>
                 <Product
+                id={1236}
                     title="Apple iPhone 14 (128 GB) - Midnight  "
                     price={66999.69}
                     image="https://m.media-amazon.com/images/I/61cwywLZR-L._AC_UY218_.jpg"
                     rating={4}
                 />
                 <Product
+                id={1237}
                     title="Atomic Habits Paperback -1 January 2018
                     by James Clear (Author) "
                     price={640.69}
@@ -23,6 +25,7 @@ function Home() {
                     rating={5}
                 />
                 <Product
+                id={1238}
                     title="Marshall Kilburn II 36W Bluetooth Portable Speaker, color - Black"
                     price={24689.69}
                     image="https://m.media-amazon.com/images/I/81r6KN6buAL._AC_UY218_.jpg"
